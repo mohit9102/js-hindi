ed34883e5bb3bda5bd22f4b699110348a44275ae
mohit9102 <always.mohitkr@gmail.com>
2024-09-12 23:23:14 +0530

    some new concept

--- .devcontainer/01_basics/05_string.js
+++ .devcontainer/01_basics/05_string.js
@@ -3,7 +3,7 @@ const repocount =50
 // console.log( `my name is ${name} and my repocount is ${repocount}`);
 
 
-const gamename = new String('mohi-tc')
+const gamename = new String('mohi-tc')// use browser console to get more information about the prototype
 
 // console.log(gamename.__proto__);
 
@@ -15,19 +15,30 @@ const gamename = new String('mohi-tc')
 // console.log(gamename.indexOf('t'));
 
 
-const newstring = gamename.substring(0,3) // end index is not included
-// console.log(newstring);
+// const newstring = gamename.substring(0,3) // end index is not included
+// // console.log(newstring);
 
-const anotherString = gamename.slice(-6,2)//in we can pass the negative value
+// const anotherString = gamename.slice(-6,2)//in we can pass the negative value
 
-console.log(anotherString);
-
-
-const newStringOne ="  hitesh   "
-console.log(newStringOne);
+// console.log(anotherString);
 
 
-console.log(newStringOne.trim());//remove the unwanted space from the string
+// const newStringOne ="  hitesh   "
+// console.log(newStringOne);
+
+
+// // console.log(newStringOne.trim());//remove the unwanted space from the string
+
+
+const url =" https://hitesh.com/hitesh%20chaudhary"//
+
+console.log(url.replace('%20','-')) 
+console.log((url.includes('SUNDAR')));
+console.log(gamename.split('-'));
+ 
+
+
+
  
 
 
---
+++ .devcontainer/01_basics/06_nums-math.js
@@ -0,0 +1,53 @@
+const score = 400
+// console.log(score);
+
+
+const balance =new Number(100)
+// console.log(balance);
+// console.log(balance.toString());// by doing this you can acsses all the function of string
+// console.log(balance.toFixed(2));//add two 0 at end
+
+const otherNumber = 23.8966
+
+// console.log(otherNumber.toPrecision(3));// return string and round off to 3digit
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+ 
+
+//+++++++++++++++++ Maths +++++++++++++++++
+
+
+// console.log(Math);// it an object and it has various property
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.3)); 
+// console.log(Math.ceil(4.3));// goes to 5
+// console.log(Math.floor(4.9));// goes to 4
+// console.log(Math.min(4,5,6,2,3));
+
+
+//console.log(Math.random());/// its value lies always between 0 and 1
+console.log((Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1))+ min );// if you want to number betwwen min and max use this 
+
+// you do +1 to avoid 0 
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
